dce467d8cf04f727af5b2aa4111931ddbaf7f5e6
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-04-21 00:32:57 +0800

    更新decoder1to2、decoder1to2withEnable decoder2to4 decoder2to4withEnable：新增include guard

--- decoder1to2withEnable/GNU_toolchain/Source_code/decoder1to2withEnable/decoder1to2withEnable.v
+++ decoder1to2withEnable/GNU_toolchain/Source_code/decoder1to2withEnable/decoder1to2withEnable.v
@@ -1,14 +1,17 @@
 /* 模組名稱：帶有Enable的1對2解碼器
    著作權宣告：copyright 2012 林博仁(pika1021@gmail.com)
    */
-`timescale 1ns / 100ps
+`ifndef DECODER1TO2WITHENABLE_V_INCLUDED	
+	`define DECODER1TO2WITHENABLE_V_INCLUDED
+	`timescale 1ns / 100ps
 
-module decoder1to2withEnable(o1, o0, i, enable);
-  input i, enable;
-  output o1, o0;
+	module decoder1to2withEnable(o1, o0, i, enable);
+		input i, enable;
+		output o1, o0;
 
-  not inv(ibar, i);
-  and and1(o1, i, enable),
-      and0(o0, ibar, enable);
+		not inv(ibar, i);
+		and and1(o1, i, enable),
+				and0(o0, ibar, enable);
 
-endmodule
+	endmodule
+`endif
--- decoder2to4/GNU_toolchain/Source_code/decoder1to2/decoder1to2.v
+++ decoder2to4/GNU_toolchain/Source_code/decoder1to2/decoder1to2.v
@@ -1,15 +1,18 @@
-/* 模組名稱：
+/* 模組名稱：1對2解碼器(decoder)
    著作權宣告：copyright 2012 林博仁(pika1021@gmail.com)
    */
-`timescale 1ns / 100ps
+`ifndef DECODER1TO2_V_INCLUDED
+	`define DECODER1TO2_V_INCLUDED
+	`timescale 1ns / 100ps
 
-module decoder1to2(o1, o0, i);
-	output o1, o0;
-	input i;
-  wire ibar;
-  
-	not
-	  inv(ibar, i);
-	assign o1 = i;
-	assign o0 = ibar;
-endmodule
+	module decoder1to2(o1, o0, i);
+		output o1, o0;
+		input i;
+		wire ibar;
+		
+		not
+			inv(ibar, i);
+		assign o1 = i;
+		assign o0 = ibar;
+	endmodule
+`endif
