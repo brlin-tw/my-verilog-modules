6c81b94c86f372cef31877f4ff46853a5106b330
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-04-20 21:35:34 +0800

    更新multiplexer4to1：將port vector化

--- multiplexer4to1/GNU_environment/Source_code/multiplexer4to1/multiplexer4to1.v
+++ multiplexer4to1/GNU_environment/Source_code/multiplexer4to1/multiplexer4to1.v
@@ -2,12 +2,13 @@
 `timescale 1ns / 100ps
 `include "Source_code/multiplexer2to1/multiplexer2to1.v"
 
-module multiplexer4to1(out,s1,s0,i0,i1,i2,i3);
+module multiplexer4to1(out, i, s);
   output out;
-  input s1,s0,i0,i1,i2,i3;
+  input [1:0] s;
+  input [3:0] i;
   wire net1,net0;
 
-  multiplexer2to1 a1(.out(net0), .i1(i1), .i0(i0), .s(s0));
-  multiplexer2to1 a2(.out(net1), .i1(i3), .i0(i2), .s(s0));
-  multiplexer2to1 b(.out(out), .i1(net1), .i0(net0), .s(s1));
+  multiplexer2to1 a1(.out(net0), .i1(i[1]), .i0(i[0]), .s(s[0]));
+  multiplexer2to1 a2(.out(net1), .i1(i[3]), .i0(i[2]), .s(s[0]));
+  multiplexer2to1 b(.out(out), .i1(net1), .i0(net0), .s(s[1]));
 endmodule
--- multiplexer4to1/GNU_environment/Source_code/multiplexer4to1_tb.v
+++ multiplexer4to1/GNU_environment/Source_code/multiplexer4to1_tb.v
@@ -1,38 +1,33 @@
 `include "Source_code/multiplexer4to1/multiplexer4to1.v"
 module multiplexer4to1test;
-  reg s1, s0, i0, i1, i2, i3;
+  reg [1:0] s;
+  reg [3:0] i;
   wire out;
   multiplexer4to1 
     mul41(.out(out), 
-          .s1(s1), 
-          .s0(s0), 
-          .i3(i3), 
-          .i2(i2), 
-          .i1(i1), 
-          .i0(i0));
+          .s(s), 
+          .i(i));
   initial 
   begin
     //初始化
     $dumpfile("Simulation/multiplexer4to1_tb.vcd");
     $dumpvars;
-    $monitor($time, " in = %b%b%b%b, select = %b%b -> out = %b", i3, i2, i1, i0, s1, s0, out);
-
-    s1 = 0;
-    s0 = 0;
-    i0 = 0;
-    i1 = 0;
-    i2 = 0;
-    i3 = 0;
-    #10 i0 = 1;
+    $monitor($time, " in = %b%b%b%b, select = %b%b -> out = %b", i[3], i[2], i[1], i[0], s[1], s[0], out);
+		s = 2'b00;
+		i = 4'b0000;
+		
+		//模擬
+    #10 i = 4'b0001;
+    //output should now 1
     
-    #10 s0 = 1;
-    #10 i1 = 1;
+    #10 s = 2'b01;
+    #10 i = 4'b0010;
     
-    #10 s1 = 1; s0 = 0; 
-    #10 i2 = 1;
+    #10 s = 2'b10;
+    #10 i = 4'b0100;
     
-    #10 s1 = 1; s0 = 1; 
-    #10 i3 = 1;
+    #10 s = 2'b11;
+    #10 i = 4'b1000;
     #10 $finish;
   end
 endmodule
