65634c71fd68acb7620f2bd422de47649513cf02
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-04-25 12:10:15 +0800

    更新fourBitDFFdownCounterWasynClock：修正不是upcount

--- fourBitDFFdownCounterWasynClock/GNU_toolchain/Source_code/dFlipFlopPosClkNegRst/dFlipFlopPosClkNegRst.v
+++ fourBitDFFdownCounterWasynClock/GNU_toolchain/Source_code/dFlipFlopPosClkNegRst/dFlipFlopPosClkNegRst.v
@@ -1,14 +1,14 @@
 /* 模組名稱： D型正反器（負緣觸發）
    著作權宣告：copyright 2012 林博仁(pika1021@gmail.com)
    */
-`ifndef DFLIPFLOPNEGCLKRST_V
-	`define DFLIPFLOPNEGCLKRST_V
+`ifndef DFLIPFLOPPOSCLKNEGRST_V
+	`define DFLIPFLOPPOSCLKNEGRST_V
 	`timescale 1ns / 100ps
-	module dFlipFlopNegClkNegRst(Q, Qbar, D, Clk, ClrN);
+	module dFlipFlopPosClkNegRst(Q, Qbar, D, Clk, ClrN);
 		input D, Clk, ClrN;
 		output reg Q, Qbar;
 		
-		always @(negedge Clk or negedge ClrN)
+		always @(posedge Clk or negedge ClrN)
 		begin
 			if(~ClrN) begin
 				Q <= 0; 
