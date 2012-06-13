638f5d301f9c6f309dfc00ec52bcb14fa12b1a8c
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-06-13 11:14:01 +0800

    無紀錄commit

--- fourBitDFFupCounterWasynClock/GNU_toolchain/Source_code/fourBitDFFupCounterWasynClock/fourBitDFFupCounterWasynClock.v
+++ fourBitDFFupCounterWasynClock/GNU_toolchain/Source_code/fourBitDFFupCounterWasynClock/fourBitDFFupCounterWasynClock.v
@@ -7,16 +7,13 @@
 		input Clk, ClrN;
 		wire w3, w2, w1, w0;
 		wire [3:0]dummy;
-		
-		dFlipFlopNegClkNegRst c0(w0, dummy[0], ~w0, Clk, ClrN), 
-		c1(w1, dummy[1], ~w1, w0, ClrN), 
-		c2(w2, dummy[2], ~w2, w1, ClrN), 
+
+		dFlipFlopNegClkNegRst c0(w0, dummy[0], ~w0, Clk, ClrN),
+		c1(w1, dummy[1], ~w1, w0, ClrN),
+		c2(w2, dummy[2], ~w2, w1, ClrN),
 		c3(w3, dummy[3], ~w3, w2, ClrN);
-		
-		assign count[0] = w0, 
-						count[1] = w1, 
-						count[2] = w2, 
-						count[3] = w3;
-		
+
+		assign count = {w3, w2, w1, w0};
+
 	endmodule
 `endif
