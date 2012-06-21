68226ddd0183776845daee339833875ab5961f93
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-06-21 10:12:23 +0800

    新增SAP-1 instruction register

---
+++ SAP_1_instruction_register/GNU_toolchain/Source_code/SAP_1_instruction_register/SAP_1_instruction_register.v
@@ -0,0 +1,33 @@
+//include guard
+`ifndef SAP_1_instruction_register
+	`define SAP_1_instruction_register
+	// 時間相關設定
+	`timescale 1ns / 100ps
+
+	`include "Source_code/flipflopDnegClknegRstWithNegLoadEnable8bit/flipflopDnegClknegRstWithNegLoadEnable8bit.v"
+
+	/* 模組名稱：SAP_1_instruction_register
+		著作權宣告：copyright 2012 林博仁(pika1021@gmail.com) */
+	module SAP_1_instruction_register(opcode, operand, instruction, LIbar, EIbar, Clk, Clr);
+	//port 輸出輸入宣告
+		output [7:4]opcode;
+		output [3:0]operand;
+		input [7:0]instruction;
+		input LIbar, EIbar, Clk, Clr;
+
+	//port 類型宣告
+		wire [7:4]opcode;
+		wire [3:0]operand;
+		wire [7:0]reg_out;
+	flipflopDnegClknegRstWithNegLoadEnable8bit
+		register(reg_out, instruction, Clk, Clr, Libar);
+
+	bufif0
+		EIbuf3(operand[3], reg_out[3], EIbar),
+		EIbuf2(operand[2], reg_out[2], EIbar),
+		EIbuf1(operand[1], reg_out[1], EIbar),
+		EIbuf0(operand[0], reg_out[0], EIbar);
+
+	assign opcode[7:4] = reg_out[7:4];
+	endmodule
+`endif
---
+++ SAP_1_instruction_register/GNU_toolchain/Source_code/SAP_1_instruction_register_tb.v
@@ -0,0 +1,31 @@
+//testbench範本
+//時間相關設定
+`timescale 1ns / 100ps
+
+//include D.U.T.模組
+
+//時脈頻率
+//`define CLOCK_FREQ 50
+
+module _tb();
+//宣告port類型
+  reg ;
+  wire ;
+
+	//D.U.T. instantiation
+
+  /* 時脈產生器
+  always begin
+    #`CLOCK_FREQ Clk = !Clk;
+	end
+  */
+
+  initial begin
+		//初始化
+		$dumpfile ("Simulation/_tb.vcd");
+		$dumpvars;
+		$monitor($time, " ");
+
+		//模擬
+	end
+endmodule
