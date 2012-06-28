073a8f89ff51aa8f70e171f1fbda66fe4103c58a
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2012-06-28 15:38:07 +0800

    [SAP_1_instruction_register]修正與設計不符問題、完成模擬

--- SAP_1_instruction_register/GNU_toolchain/Source_code/SAP_1_instruction_register/SAP_1_instruction_register.v
+++ SAP_1_instruction_register/GNU_toolchain/Source_code/SAP_1_instruction_register/SAP_1_instruction_register.v
@@ -8,19 +8,19 @@
 
 	/* 模組名稱：SAP_1_instruction_register
 		著作權宣告：copyright 2012 林博仁(pika1021@gmail.com) */
-	module SAP_1_instruction_register(opcode, operand, instruction, LIbar, EIbar, Clk, Clr);
+	module SAP_1_instruction_register(opcode, operand, instruction, LIbar, EIbar, ClkN, ClrN);
 	//port 輸出輸入宣告
 		output [7:4]opcode;
 		output [3:0]operand;
 		input [7:0]instruction;
-		input LIbar, EIbar, Clk, Clr;
+		input LIbar, EIbar, ClkN, ClrN;
 
 	//port 類型宣告
 		wire [7:4]opcode;
 		wire [3:0]operand;
 		wire [7:0]reg_out;
 	flipflopDnegClknegRstWithNegLoadEnable8bit
-		register(reg_out, instruction, Clk, Clr, Libar);
+		register(reg_out, instruction, ClkN, ClrN, LIbar);
 
 	bufif0
 		EIbuf3(operand[3], reg_out[3], EIbar),
--- SAP_1_instruction_register/GNU_toolchain/Source_code/SAP_1_instruction_register_tb.v
+++ SAP_1_instruction_register/GNU_toolchain/Source_code/SAP_1_instruction_register_tb.v
@@ -3,29 +3,52 @@
 `timescale 1ns / 100ps
 
 //include D.U.T.模組
+`include "Source_code/SAP_1_instruction_register/SAP_1_instruction_register.v"
 
 //時脈頻率
-//`define CLOCK_FREQ 50
+`define CLOCK_FREQ 10
 
-module _tb();
+module SAP_1_instruction_register_tb();
 //宣告port類型
-  reg ;
-  wire ;
+  reg Clk, Clr, LIbar, EIbar;
+  reg [7:0]instruction;
+  wire [3:0]opcode;
+  wire [3:0]operand;
 
 	//D.U.T. instantiation
+	SAP_1_instruction_register
+		dut(opcode, operand, instruction, LIbar, EIbar, Clk, Clr);
 
-  /* 時脈產生器
+  /* 時脈產生器 */
   always begin
     #`CLOCK_FREQ Clk = !Clk;
 	end
-  */
 
   initial begin
 		//初始化
-		$dumpfile ("Simulation/_tb.vcd");
+		$dumpfile ("Simulation/SAP_1_instruction_register_tb.vcd");
 		$dumpvars;
-		$monitor($time, " ");
+		$display("\t\t時間\tClk\tClr\tLIbar\tEIbar\tinstruct --> opcode\toperand");
+		$monitor("%d\t%b\t%b\t%b\t%b\t%8b --> %4b\t%4b", $time, Clk, Clr, LIbar, EIbar, instruction, opcode, operand);
+		Clk = 0;
+		Clr = 0;
+		LIbar = 1;
+		EIbar = 1;
 
 		//模擬
+		#10
+		Clr = 1;
+
+		#15
+		instruction = 8'b10101010;
+
+		#20
+		LIbar = 0;
+
+		#20
+		EIbar = 0;
+
+		#20
+		$finish;
 	end
 endmodule
