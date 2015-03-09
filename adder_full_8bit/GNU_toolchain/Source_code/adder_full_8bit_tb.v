1fb434f6477385d2b18ee371f96a8a48895c304e
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2015-03-09 14:47:34 +0800

    fullAdder[48]bit - 改善模組命名、改用符號式連結連結其他模組
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- adder_full_8bit/GNU_toolchain/Source_code/adder_full_8bit_tb.v
+++ adder_full_8bit/GNU_toolchain/Source_code/adder_full_8bit_tb.v
@@ -1,25 +1,25 @@
-//testbench of fullAdder8bit
+//testbench of adder_full_8bit
 `timescale 1ns / 100ps
 
 //include模組
-`include "Source_code/fullAdder8bit/fullAdder8bit.v"
+`include "Source_code/adder_full_8bit/adder_full_8bit.v"
 
 //時脈頻率
 //`define CLOCK_FREQ 50
 
-module fullAdder8bit_tb;
+module adder_full_8bit_tb;
   reg [7:0]add, aug;
   reg preC;
   wire proC;
   wire [7:0]sum;
 
 	//D.U.T. instantiation
-  fullAdder8bit adder(proC, sum, add, aug, preC);
+  adder_full_8bit adder(proC, sum, add, aug, preC);
 
   initial
     begin
 	    //初始化
-      $dumpfile ("Simulation/fullAdder8bit_tb.vcd");
+      $dumpfile ("Simulation/adder_full_8bit_tb.vcd");
       $dumpvars;
       $monitor($time, " add=%8b + aug=%8b + preC=%b = proC=%b + sum=%8b", add, aug, preC, proC, sum);
 
