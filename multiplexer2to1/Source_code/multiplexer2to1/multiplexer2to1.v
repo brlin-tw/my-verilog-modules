ab00b0ae6dc944c6bd87eb39bf5d646110591864
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2015-04-13 14:09:37 +0800

    multiplexer2to1 - 新增實作方式 postfix
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- multiplexer2to1/Source_code/multiplexer2to1/multiplexer2to1.v
+++ multiplexer2to1/Source_code/multiplexer2to1/multiplexer2to1.v
@@ -1,18 +1,1 @@
-//2對1多工器
-//作者 | Author
-//  林博仁(pika1021@gmail.com)
-`ifndef MULTIPLEXER2TO1_V_INCLUDED
-	`define MULTIPLEXER2TO1_V_INCLUDED
-	`timescale 1ns / 100ps
-
-	module multiplexer2to1(out,i1,i0,s);
-		input i0,i1,s;
-		output out;
-		wire net1, net2;
-		
-		not a1(sbar,s);
-		and a2(net1,i0,sbar);
-		and a3(net2,i1,s);
-		or a4(out,net1,net2);
-	endmodule
-`endif
+multiplexer2to1_gate_level.v
