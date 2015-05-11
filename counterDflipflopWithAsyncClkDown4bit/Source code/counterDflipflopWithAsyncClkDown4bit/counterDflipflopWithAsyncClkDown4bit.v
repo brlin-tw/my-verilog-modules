be19178ae0a9b3969bf5fed0ed4214d0206c5cb9
林博仁(Buo-ren, Lin) <Buo.Ren.Lin@gmail.com>
2015-05-11 16:20:29 +0800

    counterDflipflopWithAsyncClkDown4bit - fix not count down bug
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- counterDflipflopWithAsyncClkDown4bit/Source code/counterDflipflopWithAsyncClkDown4bit/counterDflipflopWithAsyncClkDown4bit.v
+++ counterDflipflopWithAsyncClkDown4bit/Source code/counterDflipflopWithAsyncClkDown4bit/counterDflipflopWithAsyncClkDown4bit.v
@@ -2,14 +2,14 @@
 `ifndef COUNTERDFLIPFLOPWITHASYNCCLKDOWN4BIT
 	`define COUNTERDFLIPFLOPWITHASYNCCLKDOWN4BIT
 	`timescale 1ns / 100ps
-	`include "../../Source code/flipflopDnegClknegRst/flipflopDnegClknegRst.v"
+	`include "../../Source code/flipflopDposClknegRst/flipflopDposClknegRst.v"
 	module counterDflipflopWithAsyncClkDown4bit(count, Clk, ClrN);
 		output [3:0]count;
 		input Clk, ClrN;
 		wire w3, w2, w1, w0;
 		wire [3:0]dummy;
 
-		flipflopDnegClknegRst c0(w0, dummy[0], ~w0, Clk, ClrN),
+		flipflopDposClknegRst c0(w0, dummy[0], ~w0, Clk, ClrN),
 		c1(w1, dummy[1], ~w1, w0, ClrN),
 		c2(w2, dummy[2], ~w2, w1, ClrN),
 		c3(w3, dummy[3], ~w3, w2, ClrN);
